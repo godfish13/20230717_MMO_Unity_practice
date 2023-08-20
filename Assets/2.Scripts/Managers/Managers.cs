eef2c27c0b1c38bae11f7604132fc0853aad28cb
godfish13 <63995325+godfish13@users.noreply.github.com>
2023-08-21 00:51:27 +0900

    20230820_Coroutine, DataManager, JSON file
    코루틴 기초, JSON파일과 그를 통한 Data의 관리 기초

--- Assets/2.Scripts/Managers/Managers.cs
+++ Assets/2.Scripts/Managers/Managers.cs
@@ -9,6 +9,7 @@ public class Managers : MonoBehaviour
     static Managers Mgr_Instance;     // 이 스크립트가 존재하지않을 때 다른곳에서 먼저 호출 시 init()한번 실행하여 singleton패턴의 instance 만들어줌
     public static Managers Instance { get { init(); return Mgr_Instance; } }  // 이미 존재할 경우 init()내에서 생성 스킵됨
 
+    DataMgr _dataMgr = new DataMgr();
     InputMgr _inputMgr = new InputMgr();
     PoolMgr _poolMgr = new PoolMgr();
     ResourceMgr _resourceMgr = new ResourceMgr();
@@ -16,6 +17,7 @@ public class Managers : MonoBehaviour
     SoundMgr _soundMgr = new SoundMgr();
     UIMgr _UIMgr = new UIMgr();
 
+    public static DataMgr dataMgr { get { return Instance._dataMgr; } }
     public static InputMgr inputMgr { get { return Instance._inputMgr; } }
     public static PoolMgr poolMgr { get { return Instance._poolMgr; } }
     public static ResourceMgr resourceMgr { get { return Instance._resourceMgr; } }
@@ -51,6 +53,7 @@ public class Managers : MonoBehaviour
             DontDestroyOnLoad(MgrObject);
             Mgr_Instance = MgrObject.GetComponent<Managers>();
 
+            Mgr_Instance._dataMgr.init();
             Mgr_Instance._poolMgr.init();
             Mgr_Instance._soundMgr.init();
         }      
@@ -58,6 +61,7 @@ public class Managers : MonoBehaviour
 
     public static void Clear()
     {
+        // DataMgr은 크기도 작고 늘 존재해야하므로 Clear안함
         inputMgr.Clear();
         sceneMgrEx.Clear();
         soundMgr.Clear();
--- Assets/2.Scripts/Scenes/InGameScene.cs
+++ Assets/2.Scripts/Scenes/InGameScene.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class InGameScene : BaseScene
 {
+    Coroutine co;
+
     protected override void init()
     {
         base.init();
@@ -12,10 +14,34 @@ public class InGameScene : BaseScene
         //Managers.UIMgr.ShowSceneUI<UI_Inven>();
         //Managers.UIMgr.ShowPopUpUI<UI_Btn>();   
 
-        for (int i = 0; i < 4; i++)
+        /*for (int i = 0; i < 4; i++)
         {
             Managers.resourceMgr.Instantiate("UnityChan");
+        }*/
+
+        /*co = StartCoroutine("CoExplode", 4.0f);     // 이런모양으로도 사용가능
+        StopCoroutine(CoStopExplode(2.0f));*/
+
+        Dictionary<int, Stat> dict = Managers.dataMgr.StatDictionary;
+    }
+
+    IEnumerator CoStopExplode(float seconds)
+    {
+        Debug.Log("Stop Enter...");
+        yield return new WaitForSeconds(seconds);
+        Debug.Log("Exlode Stopped");
+        if (co != null)
+        {
+            StopCoroutine(co);
+            co = null;
         }
+    }
+
+    IEnumerator CoExplode(float seconds)
+    {
+        Debug.Log("Explode Enter...");
+        yield return new WaitForSeconds(seconds);
+        Debug.Log("KABOOM!");
     }
 
     public override void Clear()
